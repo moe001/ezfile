5549c946e8b4992f0081e869496e2889f2901f2c
moe001 <i@001.moe>
2017-01-30 23:34:41 +0800

    update version

--- ezfile_main/setup.py
+++ ezfile_main/setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='ezfile',
-      version='0.0.2',
+      version='0.0.3',
       description='easy file-io',
       author='moe001',
       author_email='i@001.moe',
